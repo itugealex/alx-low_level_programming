3afda46080167c8ca45c604f63c106386ec4fcf9
itugealex <itugelaex@gmail.com>
2022-06-02 19:21:22 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
  * main - Puts "\"Programming is like bulding a multilingual puzzle"
  *
  * Description: returns a string
+ *
+ * Return: 0 if code is ok
  */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,10 @@
  *main - prints "with proper grammar, but the outcome is a piece of art," .
  *
  * Description: entry file.
+ *
+ * Return: 0 if the code is ok
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
