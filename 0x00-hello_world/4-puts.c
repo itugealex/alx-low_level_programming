c423e11144ea6ada9eb765dfe4af4f842b32a59c
itugealex <itugelaex@gmail.com>
2022-06-02 19:08:34 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 
 /**
- * main - code entry point
+ * main - Prints "\"Programming is like bulding a multilingual puzzle"
+ *
+ * Description: returns a string
  */
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
