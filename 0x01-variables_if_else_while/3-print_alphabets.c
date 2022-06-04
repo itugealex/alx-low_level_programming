6a42f02ef1afb009c23675a6b7bb17bd6d33cf9f
itugealex <itugelaex@gmail.com>
2022-06-03 18:46:40 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - code entry point
+ *
+ * Description: Fourth task
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch)
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
