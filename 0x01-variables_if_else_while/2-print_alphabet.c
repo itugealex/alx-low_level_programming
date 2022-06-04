2e3cce5bac348095cf66ea3df2de422452c84a76
itugealex <itugelaex@gmail.com>
2022-06-03 19:37:06 -0700

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,14 @@
  *
  * Description: third task
  *
+ *Return: Returns zero
+ *
  */
 int main(void)
 {
 	int x;
 
-	for(x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 		putchar(x);
 	putchar('\n');
 	return (0);
