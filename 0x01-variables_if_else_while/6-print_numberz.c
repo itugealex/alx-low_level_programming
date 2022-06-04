31de9b69bc995543079ffefbd98df6d2bda67f24
itugealex <itugelaex@gmail.com>
2022-06-03 19:12:29 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Seventh task
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+		putchar(number);
+	putchar('\n');
+
+	return (0);
+}
