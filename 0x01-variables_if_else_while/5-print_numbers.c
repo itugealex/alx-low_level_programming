120080f56b9fe22300f4ac075920f5922b5fbcb1
itugealex <itugelaex@gmail.com>
2022-06-03 19:00:59 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - code entry point
+ *
+ * Description: Sixth task
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	printf("\n");
+
+	return (0);
+}
