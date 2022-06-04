2a773acbde0e611296568da436cc530e05ffcdb9
itugealex <itugelaex@gmail.com>
2022-06-03 18:28:17 -0700

    some stuff

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main class
+ *
+ * Description: third task
+ *
+ */
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+		int lower_x = tolower(x);
+		putchar(lower_x);
+	}
+	return (0);
+}
