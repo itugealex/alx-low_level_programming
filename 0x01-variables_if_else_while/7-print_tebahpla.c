ad7b31c4736a5bd80bbacd31f07d4093788bf8a9
itugealex <itugelaex@gmail.com>
2022-06-03 19:23:27 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Eight task
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	retun (0);
+}
