6b39d9e23d8ec829201debc9b339fb0b02afee5f
itugealex <itugelaex@gmail.com>
2022-06-08 10:20:50 -0700

    minor changes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Code entry point.
+ *
+ * Description: code description.
+ *
+ * Return: Returns zero.
+ */
+
+int main(void)
+{
+	char i;
+	for( i = 0; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
