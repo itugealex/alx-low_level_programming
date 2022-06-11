2a8ccced8429677638cedbf74583c48fb1512f81
itugealex <itugelaex@gmail.com>
2022-06-10 19:58:56 -0700

    minorchanges

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+/*prints _putchar*/
+int _putchar(char c);
+
+/* function checks for uppercase letters*/
+int _isupper(int c);
+
+/*function checks for digit*/
+int _isdigit(int  c);
+
+/* functions that multiply two integers*/
+int mul(int a, int b);
+
+/*function that prints the numbers 0 to 9*/
+void print_numbers(void);
+
+/*function that prints the numbers*/
+void more_numbers(void);
+
+/*function that draws a straight line*/
+void print_line(int n);
+
+/*function that draw a diagonal line*/
+void print_diagonal(int n);
+
+/*print the squares*/
+void print_square(int size);
+
+/*function prints a triangle*/
+void print_triangle(int size);
+
+/*function prints an integer*/
+void print_number(int n);
