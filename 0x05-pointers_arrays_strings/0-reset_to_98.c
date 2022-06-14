876d688b9278a002971ec7d5037e07017562d2a4
itugealex <itugelaex@gmail.com>
2022-06-13 18:27:10 -0700

    minor changes

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - fucntion takes input and updates the value it points to.
+ *
+ * @n: pointer n
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+int _putchar char(c);
+
+/* This function points to an int as parameter*/
+void reset_to_98(int *n);
