75feb07b612e55a2b0e7548705282a1500e00414
itugealex <itugelaex@gmail.com>
2022-06-13 18:38:38 -0700

    minor changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,35 @@
 /*prints _putchar*/
 int _putchar (char c);
 
-/* This function points to an int as parameter*/
+/* function that points to a int as parameter*/
 void reset_to_98(int *n);
 
-/*This functions switches two pointers*/
+/*function that swaps the values of two ints*/
 void swap_int(int *a, int *b);
+
+/* function that returns the length of a string*/
+int _strlen(char *s);
+
+/*function that prints a string */
+void _puts(char *str);
+
+/* function that prints a string in reverse*/
+void print_rev(char *s);
+
+/*function that reverses a string*/
+void rev_string(char *s);
+
+/*function prints every other character of a string*/
+void puts2(char *str);
+
+/*function that prints half of a string*/
+void puts_half(char *str);
+
+/*function prints n elements of an array of integers*/
+void print_array(int *a, int n);
+
+/*function that copies the string pointed to by src*/
+char *_strcpy(char *dest, char *src);
+
+/*function that converts a string to an integer*/
+int _atoi(char *s);
