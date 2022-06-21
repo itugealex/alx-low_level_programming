6282b6a5615e1909cc6011b005f34dde2fb48f83
itugealex <itugelaex@gmail.com>
2022-06-20 18:02:05 -0700

    minor changes

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+/*prints putchar*/
+int _putchar (char c);
+
+/*function that filss memeory with a constant byte*/
+char *_memset(char *s, char b, unsigned int n);
+
+/*function that copies memory area*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* function that locates a character in a string*/
+char *_strchr(char *s, char c);
+
+/*function that gets the length of a prefix*/
+unsigned int _strspn(char *s, char *accept);
+
+/*finctuon that searches a string for any of a set of bytes*/
+char *_strpbrk(char *s, char *accept);
+
+/*funtion htat locates a substring*/
+char *_strstr(char *haystack, char *needle);
+
+/*funciton that prints the chessboard*/
+void print_chessboard(char (*a)[8]);
+
+/*function that prints the sum of the diagonals of a square matrix*/
+void print_diagsums(int *a, int size);
+
+/*function that sets the value of a pointer to a char*/
+void set_string(char **s, char *to);
