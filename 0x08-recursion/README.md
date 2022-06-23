21a370264a161a5fe3b27ca31893bd24d4e81c95
itugealex <itugelaex@gmail.com>
2022-06-22 19:28:13 -0700

    minor changes

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recusrion and arrays
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,29 @@
+/*prints putchar*/
+int _putchar (char c);
+
+/*function prints a string*/
+void _puts_recursion(char *s);
+
+/*function prints a string in reverse*/
+void _print_rev_recursion(char *s);
+
+/*function that returns the length of a string*/
+int _strlen_recursion(char *s);
+
+/*function that returns the factorial of a given number*/
+int factorial(int n);
+
+/*function that returns the value of x raised to y*/
+int _pow_recursion(int x, int y);
+
+/*function that returns the natural square root*/
+int _sqrt_recursion(int n);
+
+/*function returns 1 of the input is a prime number*/
+int is_prime_number(int n);
+
+/*function returns 1*/
+int is_palindrome(char *s);
+
+/*function that compares two strings*/
+int wildcmp(char *s1, char *s2);
